0fe3c126040f6f6516e916b290a12d6dec3bbb7e
Mohammed Zaher <mohammedzaherr0@gmail.com>
2024-03-25 06:02:25 +0200

    feat: Update list.php with Bootstrap styling and Vue.js functionality

--- app/Views/list.php
+++ app/Views/list.php
@@ -1,14 +1,83 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?= $data['title']?></title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <title><?= $data['title']?></title>
 </head>
+
 <body>
-	<h1>Welcome to my <?= $data['title']?>!</h1>
-		<pre>
-		<?php var_dump($data); ?>
-		</pre>
+  <header>
+    <div class="container">
+      <div class="d-flex flex-row justify-content-between col-md-12 p-3 pb-1">
+        <div>
+          <h1 class="page-header"> <?php echo $title; ?> </h1>
+        </div>
+        <div class="page-btn-container">
+          <a href="/addproduct" class="btn btn-primary m-3"> ADD </a>
+          <button type="button" v-if="true" @click.prevent="deleteProducts" class="btn btn-danger m-3"
+            id="delete-product-btn"> MASS DELETE </button>
+        </div>
+      </div>
+      <hr class="m-1">
+    </div>
+  </header>
+  <div id="app">
+    <main>
+      <div class="container">
+        <div class="d-flex flex-wrap col-m d-12">
+          <div class="row">
+            <div v-for="product in products" :key="product.id" class="col-md-6 col-lg-3 mt-4 mb-4">
+              <div class="product-card">
+                <div class="card text-center border-black pb-5">
+                  <div class="card-body d-flex flex-column">
+                    <div class="d-flex justify-content-start mb-2">
+                      <input class="delete-checkbox" :id="product.id" type="checkbox" value="">
+                    </div>
+                    <div> {{ product.sku }} </div>
+                    <div> {{ product.name }} </div>
+                    <div> {{ product.price }} $ </div>
+                    <div> {{ product.details }} </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+
+  <footer id="footer">
+    <div class="container">
+      <hr class="m-1 mb-4">
+      <p class="text-center"> Scandiweb Test Assignment </p>
+    </div>
+  </footer>
+
+  <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+  <script type="module">
+  let app = Vue.createApp({
+    data() {
+      return {
+        products: <?= json_encode($data["products"]) ?>
+      }
+    },
+    methods: {
+      addProduct() {
+        window.location.href = window.location.origin + "/addProduct/"
+      },
+    }
+  })
+  app.mount('#app')
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+  </script>
 </body>
+
 </html>
